2861959ef9a4a0bb6346acb9c84c2ffc54e2b8f0
dacadla <71786326+dacadla@users.noreply.github.com>
2022-12-31 21:25:18 +0100

    make a new command

---
+++ commands/hello.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('hello')
+		.setDescription('Says hello to the bot.'),
+	async execute(interaction) {
+		// interaction.user is the object representing the User who ran the command
+		// interaction.member is the GuildMember object, which represents the user in the specific guild
+		await interaction.reply(`Hi there, ${interaction.user.username}`);
+	},
+};
